9e6ce0fddbcdfd53cce9a8959b76b370981b7af1
BWallyn <ben96.wallyn@gmail.com>
2025-03-07 20:33:26 +0100

    feat(data): Pipeline to download data

--- src/stock_data/pipelines/download_data/pipeline.py
+++ src/stock_data/pipelines/download_data/pipeline.py
@@ -3,8 +3,26 @@ This is a boilerplate pipeline 'download_data'
 generated using Kedro 0.19.11
 """
 
-from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from kedro.pipeline import node, Pipeline, pipeline
+
+from stock_data.pipelines.download_data.nodes import download_data_range
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        pipe=[
+            node(
+                func=download_data_range,
+                inputs=[
+                    "params:stock_name_aapl",
+                    "params:start_data",
+                    "params:end_date",
+                ],
+                outputs="stock_data_aapl",
+                name="Download_data_range",
+            ),
+        ],
+        namespace="Download_data",
+        inputs=None,
+        outputs="stock_data_aapl",
+    )
