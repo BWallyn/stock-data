9b8db58b97239a8b32a55458e8b95ef070ab0b09
BWallyn <ben96.wallyn@gmail.com>
2025-04-06 17:59:28 +0200

    refactor: Change the definition of test set in number of days

--- src/stock_data/pipelines/feature_engineering/nodes.py
+++ src/stock_data/pipelines/feature_engineering/nodes.py
@@ -10,22 +10,25 @@ import pandas as pd
 # ==== FUNCTIONS ====
 # ===================
 
-def add_test_indicator(df: pd.DataFrame, date_test: str) -> pd.DataFrame:
+def add_test_indicator(df: pd.DataFrame, n_days: str) -> pd.DataFrame:
     """Add a test indicator column to the DataFrame.
 
     Args:
         df (pd.DataFrame): Input DataFrame.
-        date_test (str): Date to use for the test indicator.
+        n_days (int): Number of days to use for the test indicator.
 
     Returns:
         (pd.DataFrame): DataFrame with the test indicator column added.
 
     """
-    df["test"] = df["date"] >= date_test
+    df["test"] = df["date"].between(
+        df["date"].max() - pd.Timedelta(days=n_days),
+        df["date"].max()
+    )
     return df
 
 
-def add_validation(df: pd.DataFrame, n_days: int) -> pd.DataFrame:
+def add_validation_indicator(df: pd.DataFrame, n_days: int) -> pd.DataFrame:
     """Add a validation indicator column to the DataFrame.
 
     Args:
