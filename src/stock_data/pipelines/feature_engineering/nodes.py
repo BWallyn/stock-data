6ce6e1470e949bb00da561821fe0c6729874ca57
BWallyn <ben96.wallyn@gmail.com>
2025-04-07 08:37:50 +0200

    feat: Function to create validation indicator folds

--- src/stock_data/pipelines/feature_engineering/nodes.py
+++ src/stock_data/pipelines/feature_engineering/nodes.py
@@ -18,7 +18,7 @@ def add_test_indicator(df: pd.DataFrame, n_days: str) -> pd.DataFrame:
         n_days (int): Number of days to use for the test indicator.
 
     Returns:
-        (pd.DataFrame): DataFrame with the test indicator column added.
+        df (pd.DataFrame): DataFrame with the test indicator column added.
 
     """
     df["test"] = df["date"].between(
@@ -36,11 +36,31 @@ def add_validation_indicator(df: pd.DataFrame, n_days: int) -> pd.DataFrame:
         n_days (int): Number of days to use for the validation indicator.
 
     Returns:
-        (pd.DataFrame): DataFrame with the validation indicator column added.
+        df (pd.DataFrame): DataFrame with the validation indicator column added.
 
     """
     df["validation"] = df["date"].between(
         df[df["test"] == 0]["date"].max() - pd.Timedelta(days=n_days),
         df[df["test"] == 0]["date"].max()
     )
+    return df
+
+
+def add_validation_indicators_folds(df: pd.DataFrame, n_days: int, n_folds: int) -> pd.DataFrame:
+    """Add validation indicators for multiple folds to the DataFrame.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+        n_days (int): Number of days to use for the validation indicator.
+        n_folds (int): Number of folds to create.
+
+    Returns:
+        df (pd.DataFrame): DataFrame with the validation indicators.
+
+    """
+    for fold in range(n_folds):
+        df[f"validation_{fold}"] = df["date"].between(
+            df[df["test"] == 0]["date"].max() - pd.Timedelta(days=n_days * (fold + 1)),
+            df[df["test"] == 0]["date"].max() - pd.Timedelta(days=n_days * fold)
+        )
     return df
